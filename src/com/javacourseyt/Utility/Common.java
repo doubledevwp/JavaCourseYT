a00976023853ae55090f341c1db92051e4d0be38
doubledevwp <doubledevwp@gmail.com>
2022-08-23 22:03:25 -0400

    Adding the user input lesson; Adding in a side helper utility class to add a tryParse and some colors in the terminal

---
+++ src/com/javacourseyt/Utility/Common.java
@@ -0,0 +1,18 @@
+package com.javacourseyt.Utility;
+
+public class Common {
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_NEWLINE = "\n\n";
+
+    public static Integer tryParse(String text) {
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            System.out.println(ANSI_NEWLINE + ANSI_RED + e + ANSI_RESET);
+            System.out.println(ANSI_RED + "Invalid selection.  Must be a number." + ANSI_RESET);
+            return 0;
+        }
+    }
+}
--- src/com/javacourseyt/userinput/Main.java
+++ src/com/javacourseyt/userinput/Main.java
@@ -1,6 +1,7 @@
 package com.javacourseyt.userinput;
 
 import java.util.Scanner;
+import com.javacourseyt.Utility.Common;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,8 +12,16 @@ public class Main {
         System.out.println("Hello " + name);
 
         System.out.print("How old are you? ");
-        int age = scanner.nextInt();
+        int age = Common.tryParse(scanner.next());
         System.out.println("You're " + age + " years old!");
+        
+        if (age < 18) {
+            System.out.println("WOW! You're young!");
+        } else if (age >= 18 && age < 21) {
+            System.out.println("You can join the military!  Go be patriotic!");
+        } else if (age >= 21) {
+            System.out.println("FINALLY!  How is adulting?!");
+        }        
 
         System.out.print("What is your favorite food? ");
         String food = scanner.next();
