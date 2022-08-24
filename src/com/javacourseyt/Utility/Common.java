965a72f233c956b5c97ce8aacb344eb8051ed8b3
doubledevwp <doubledevwp@gmail.com>
2022-08-23 23:04:19 -0400

    Refactored the tryParse to tryParseInt; Added tryParseDouble; Added ERROR_MESSAGE and set the error message to display on the GUI lesson

--- src/com/javacourseyt/Utility/Common.java
+++ src/com/javacourseyt/Utility/Common.java
@@ -5,14 +5,27 @@ public class Common {
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_NEWLINE = "\n\n";
+    public static String ERROR_MESSAGE = "";
 
-    public static Integer tryParse(String text) {
+    public static Integer tryParseInt(String text) {
         try {
             return Integer.parseInt(text);
         } catch (NumberFormatException e) {
             System.out.println(ANSI_NEWLINE + ANSI_RED + e + ANSI_RESET);
             System.out.println(ANSI_RED + "Invalid input.  Must be a number." + ANSI_RESET);
+            ERROR_MESSAGE = "Invalid input.  Must be a number.";
             return 0;
+        }
+    }
+
+    public static Double tryParseDouble(String text) {
+        try {
+            return Double.parseDouble(text);
+        } catch (NumberFormatException e) {
+            System.out.println(ANSI_NEWLINE + ANSI_RED + e + ANSI_RESET);
+            System.out.println(ANSI_RED + "Invalid input.  Must be a number or decimal." + ANSI_RESET);
+            ERROR_MESSAGE = "Invalid input.  Must be a number or decimal.";
+            return 0.0;
         }
     }
 }
