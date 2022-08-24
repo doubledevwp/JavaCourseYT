567473bf912f379f2cb724c205bd5a1652825b10
doubledevwp <doubledevwp@gmail.com>
2022-08-23 22:39:23 -0400

    Added the Expressions lesson; Added the new lesson to the menu

---
+++ src/com/javacourseyt/Expressions/Main.java
@@ -0,0 +1,41 @@
+package com.javacourseyt.Expressions;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+import com.javacourseyt.Utility.Common;
+
+public class Main {
+    public static void main(String[] args) {
+        int friends = 11;
+        System.out.format("We are starting off with %d freinds." + Common.ANSI_NEWLINE, friends);
+        
+        friends++;
+        System.out.format("YAY! We found a new friend: [INC] %d" + Common.ANSI_NEWLINE, friends);
+
+        friends--;
+        System.out.format("OH NO! We lost a friend: [DEC] %d" + Common.ANSI_NEWLINE, friends);
+
+        friends += 4;
+        System.out.format("We merged with another group of friends: [ADD] %d" + Common.ANSI_NEWLINE, friends);
+
+        friends -= 2;
+        System.out.format("Some friends went wandering: [SUB] %d" + Common.ANSI_NEWLINE, friends);
+
+        friends *= 2;
+        System.out.format("OH BOY! We found more friends: [MUL] %d" + Common.ANSI_NEWLINE, friends);
+
+        friends /= 3;
+        System.out.format("Oh man! We lost our friends again: [DIV] %d" + Common.ANSI_NEWLINE, friends);
+
+        friends %= 2;
+        System.out.format("Can we make even teams? [MOD] %d" + Common.ANSI_NEWLINE, friends);
+
+        if (friends == 0) {
+            System.out.println("Yes we can!");
+        } else {
+            System.out.println("No we cannot!");
+        }
+
+    }
+    
+}
--- src/com/javacourseyt/Menu/Main.java
+++ src/com/javacourseyt/Menu/Main.java
@@ -10,9 +10,10 @@ public class Main {
 
         do {
             System.out.println("Choose from the menu options:");
-            System.out.println("\t1 -  Integers");
-            System.out.println("\t2 - Variables");
+            System.out.println("\t1 -  Variables");
+            System.out.println("\t2 - Swap Variables");
             System.out.println("\t3 - User Input");
+            System.out.println("\t4 - Expressions");
             System.out.println("\t999 - Exit");
             System.out.print("Choice: ");
 
@@ -22,17 +23,23 @@ public class Main {
 
             switch (choice) {
                 case 1:
-                    System.out.println("Calling Integers");
+                    System.out.println("Calling Variables");
                     com.javacourseyt.variables.Main.main(null);
                     break;
                 case 2:
-                    System.out.println("Calling Variables");
+                    System.out.println("Calling Swap Variables");
                     com.javacourseyt.swapvariables.Main.main(null);
                     break;
                 case 3:
                     System.out.println("Calling User Input");
                     com.javacourseyt.userinput.Main.main(null);
                     break;
+                case 4:
+                    System.out.println("Calling Expressions");
+                    com.javacourseyt.Expressions.Main.main(null);
+                    break;
+                
+                
                 case 999:
                     System.out.println(Common.ANSI_GREEN + "We hope you had a good time!\nPlease come again soon!" + Common.ANSI_RESET);
                     break;
