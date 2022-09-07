e598352e6d8a9000a5d555fe73c2b0f5ffd39584
doubledevwp <doubledevwp@gmail.com>
2022-09-06 23:54:33 -0400

    Common - updated the tryParse methods to accept objects and then do their parsing with strings.  Also have an error message with a getter and setter. GUI Main - use while loops to wait for valid input for age and height.  Also display the error message in a GUI pane

--- src/com/javacourseyt/GUI/Main.java
+++ src/com/javacourseyt/GUI/Main.java
@@ -8,18 +8,24 @@ public class Main {
         String name = JOptionPane.showInputDialog("Enter your name");
         JOptionPane.showMessageDialog(null, "Hello " + name);
 
-        int age = Common.tryParseInt(JOptionPane.showInputDialog("Enter your age"));
-        if (age > 0) {
-            JOptionPane.showMessageDialog(null, "You are " + age + " years old");
-        } else {
-            JOptionPane.showMessageDialog(null, Common.ERROR_MESSAGE);
+        Integer age = null;
+        while (age == null) {
+            age = Common.tryParseInt(JOptionPane.showInputDialog("Enter your age"));
+            if (age instanceof Integer) {
+                JOptionPane.showMessageDialog(null, "You are " + age + " years old");
+            } else {
+                JOptionPane.showMessageDialog(null, Common.GetErrorMessage());
+            }
         }
 
-        double height = Common.tryParseDouble(JOptionPane.showInputDialog("Enter your height"));
-        if (height > 0.0) {
-            JOptionPane.showMessageDialog(null, "You are " + height + " inches tall");
-        } else {
-            JOptionPane.showMessageDialog(null, Common.ERROR_MESSAGE);
+        Double height = null;
+        while (height == null) {
+            height = Common.tryParseDouble(JOptionPane.showInputDialog("Enter your height"));
+            if (height instanceof Double) {
+                JOptionPane.showMessageDialog(null, "You are " + height + " inches tall");
+            } else {
+                JOptionPane.showMessageDialog(null, Common.GetErrorMessage());
+            }
         }
     }
 }
--- src/com/javacourseyt/Utility/Common.java
+++ src/com/javacourseyt/Utility/Common.java
@@ -1,31 +1,44 @@
 package com.javacourseyt.Utility;
 
+import javax.xml.stream.events.EndElement;
+
 public class Common {
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_NEWLINE = "\n\n";
-    public static String ERROR_MESSAGE = "";
 
-    public static Integer tryParseInt(String text) {
+    private static String ERROR_MESSAGE;
+
+    public static String GetErrorMessage() {
+        return ERROR_MESSAGE;
+    }
+    
+    private static void SetErrorMessage(String message) {
+        ERROR_MESSAGE = message;
+    }
+
+    public static Integer tryParseInt(Object obj) {
+        SetErrorMessage("");
         try {
-            return Integer.parseInt(text);
+            return Integer.parseInt(obj.toString());
         } catch (NumberFormatException e) {
             System.out.println(ANSI_NEWLINE + ANSI_RED + e + ANSI_RESET);
             System.out.println(ANSI_RED + "Invalid input.  Must be a number." + ANSI_RESET);
-            ERROR_MESSAGE = "Invalid input.  Must be a number.";
-            return 0;
+            SetErrorMessage("Invalid input.  Must be a number.");
+            return null;
         }
     }
 
-    public static Double tryParseDouble(String text) {
+    public static Double tryParseDouble(Object obj) {
+        SetErrorMessage("");
         try {
-            return Double.parseDouble(text);
+            return Double.parseDouble(obj.toString());
         } catch (NumberFormatException e) {
             System.out.println(ANSI_NEWLINE + ANSI_RED + e + ANSI_RESET);
             System.out.println(ANSI_RED + "Invalid input.  Must be a number or decimal." + ANSI_RESET);
-            ERROR_MESSAGE = "Invalid input.  Must be a number or decimal.";
-            return 0.0;
+            SetErrorMessage("Invalid input.  Must be a number or decimal.");
+            return null;
         }
     }
 }
