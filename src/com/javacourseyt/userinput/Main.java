3b5665f8413893ffd55d8896ec4039386bad2d15
doubledevwp <doubledevwp@gmail.com>
2022-09-08 01:00:00 -0400

    Refactored the User Input code for error handling

--- src/com/javacourseyt/userinput/Main.java
+++ src/com/javacourseyt/userinput/Main.java
@@ -5,26 +5,30 @@ import com.javacourseyt.Utility.Common;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
 
-        System.out.print("What is your name? ");
-        String name = scanner.nextLine();
-        System.out.println("Hello " + name);
+            System.out.print("What is your name? ");
+            String name = scanner.nextLine();
+            System.out.println("Hello " + name);
 
-        System.out.print("How old are you? ");
-        int age = Common.tryParseInt(scanner.next());
-        System.out.println("You're " + age + " years old!");
-        
-        if (age < 18) {
-            System.out.println("WOW! You're young!");
-        } else if (age >= 18 && age < 21) {
-            System.out.println("You can join the military!  Go be patriotic!");
-        } else if (age >= 21) {
-            System.out.println("FINALLY!  How is adulting?!");
-        }        
+            Integer age = null;
+            while (age == null) {
+                System.out.print("How old are you? ");
+                age = Common.tryParseInt(scanner.next());
+            }
+            System.out.println("You're " + age + " years old!");
 
-        System.out.print("What is your favorite food? ");
-        String food = scanner.next();
-        System.out.println("You like " + food + "!");
+            if (age < 18) {
+                System.out.println("WOW! You're young!");
+            } else if (age >= 18 && age < 21) {
+                System.out.println("You can join the military!  Go be patriotic!");
+            } else if (age >= 21) {
+                System.out.println("FINALLY!  How is adulting?!");
+            }
+
+            System.out.print("What is your favorite food? ");
+            String food = scanner.next();
+            System.out.println("You like " + food + "!");
+        }
     }
 }
